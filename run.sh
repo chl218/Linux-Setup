e6f44c8997f137983589675d6a9600d695fcc371
jack <chl218@eng.ucsd.edu>
2017-09-13 17:17:31 -0700

    added softwares to install

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo apt-get install -y git
+sudo apt-get install -y vim
+sudo apt-get install -y openssh-server
+
+sudo apt-get install -y python-pip 
 
 cp -R .vim ~/
 cp -R .vimrc ~/
